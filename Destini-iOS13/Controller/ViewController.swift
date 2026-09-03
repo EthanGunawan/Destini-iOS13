95e0abb6215cf6ca1955a3cf0cdd04947f49ebf8
EthanGunawan <gunawane@sheridancollege.ca>
2026-09-03 10:05:21 -0400

    structure and data complete

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,7 +14,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let story0 = "You see a fork in the road..."
+    let choice1 = "Take the left path"
+    let choice2 = "Take the right path"
+    
+    let example = Story(story: "You see a fork in the road...", choice1: "Take the left path", choice2: "Take the right path"
+    )
     override func viewDidLoad() {
+        storyLabel.text = story0
+        choice1Button.setTitle(choice1, for: .normal)
+        choice2Button.setTitle(choice2, for: .normal)
         super.viewDidLoad()
 
     }
