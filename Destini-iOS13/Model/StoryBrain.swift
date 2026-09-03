66eef1b6b221dc498e62014acc20c17d8d915fe2
EthanGunawan <gunawane@sheridancollege.ca>
2026-09-03 10:14:50 -0400

    StoryBrain created

--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -8,4 +8,11 @@
 
 import Foundation
 
-
+struct StoryBrain {
+    let story = [
+        Story(t: "You see a fork in the road...", c1: "Take the left path", c2: "Take the right path"),
+        Story(t: "You find a military facility...", c1: "Go inside", c2: "Keep going"),
+        Story(t:"Godzilla attacks the city...", c1: "Fight Godzilla using Mechagodzilla", c2: "Run away"),
+        Story(t: "You find a vault ull of cash, gold and diamonds...", c1: "Take all the money", c2: "Keep going")
+    ]
+}
